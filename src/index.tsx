2eb85e7fa3a8f54c7c091957f0b20e005ebbeed7
yeosiowvic <victoryeo1974@gmail.com>
2022-12-27 20:41:25 +0800

    fix: Unexpected token, expected "," document.getElementById("root") as HTMLElement

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import ReactDOM from "react-dom/client";
-import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import theme from './theme';
 
-let htmlholder: HTMLElement = document.getElementById("root");
+let htmlholder: HTMLElement = document.getElementById("root") || new HTMLElement();
 const root = ReactDOM.createRoot(htmlholder);
 
 root.render(
