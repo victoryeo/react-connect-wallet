dde9a5e6507ba5b07843c4b415248d00e1268d69
yeosiowvic <victoryeo1974@gmail.com>
2022-12-28 18:41:27 +0800

    add block native dapp id and infura key

--- src/config.ts
+++ src/config.ts
@@ -5,3 +5,6 @@ export const collectionId = process.env.REACT_APP_COLLECTION_ID;
 export const networkId = process.env.REACT_APP_NETWORK_ID;
 export const networkName = process.env.REACT_APP_NETWORK_NAME;
 export const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+
+export const bncDappId = process.env.REACT_APP_BNC_DAPP_ID;
+export const infuraKey = process.env.REACT_APP_INFURA_KEY;
